4abf6deea2ee073bc666079d1cc834180efce1cc
heundlespeare <111425927+heundlespeare@users.noreply.github.com>
2022-08-19 06:14:22 -0700

    fixed taishanese response parsing, actually.

--- lib/dictionaries/taishanese.py
+++ lib/dictionaries/taishanese.py
@@ -11,46 +11,57 @@ class TaishaneseDict():
     def _search(self, data) -> list[str]:
         r = requests.post(self.url, data)
         r.encoding = 'utf-8'
-        html = r.text
-        soup = BeautifulSoup(html, "lxml")
-        result = soup.get_text(separator="\n", strip=True)
-        result = result.split("\n")
-        result = result[2:]
-        curr = 0
+        html = r.text.split("<hr>")
+        soups = [BeautifulSoup(h, "lxml") for h in html]
+        results = []
+        if len(soups) > 0:
+            result = soups[0].get_text("\n", strip=True)
+            result = result.split("\n")
+            result = result[2:]
+            results.append(result)
+        for soup in soups[1:]:
+            result = soup.get_text("\n", strip=True)
+            result = result.split("\n")
+            results.append(result)
         entries=[]
-        while curr < len(result):
-            word_taishan = result[curr]
-            curr +=2
-            ipa = result[curr]
-            curr += 2
-            if result[curr] == 'mandarin:':
-                canto = ''
-                curr += 1
-            else:
-                canto = result[curr]
+        
+        for result in results:
+            if len(result) < 7:
+                continue
+            curr = 0
+            while curr < len(result):
+                word_taishan = result[curr]
+                curr +=2
+                ipa = result[curr]
                 curr += 2
-            if result[curr] == 'english:':
-                mando = ''
-                curr += 1
-            else:
-                mando = result[curr]
-                curr += 2
-            if re.search('[^\u4e00-\u9fff]', result[curr]):
-                definition = result[curr]
-                curr += 1
-                while curr < len(result) and re.search('[^\u4e00-\u9fff]', result[curr]):
-                    definition += '\n' + result[curr]
-                    curr +=1
+                if result[curr] == 'mandarin:':
+                    canto = ''
+                    curr += 1
+                else:
+                    canto = result[curr]
+                    curr += 2
+                if result[curr] == 'english:':
+                    mando = ''
+                    curr += 1
+                else:
+                    mando = result[curr]
+                    curr += 2
+                if curr < len(result):
+                    definition = result[curr]
+                    curr += 1
+                    while curr < len(result):
+                        definition += ' ' + result[curr]
+                        curr +=1
+                    else:
+                        pass
                 else:
                     pass
-            else:
-                pass
-            entry = f'**{word_taishan}**({ipa})\n ' \
-                f'  Mandarin: {mando}\n'\
-                f'  Cantonese: {canto}\n\n'\
-                f'  Definition: {definition}\n'\
-                '__________'
-            entries.append(entry)
+                entry = f'**{word_taishan}**({ipa})\n ' \
+                    f'  Mandarin: {mando}\n'\
+                    f'  Cantonese: {canto}\n\n'\
+                    f'  Definition: {definition}\n'\
+                    '__________'
+                entries.append(entry)
         return entries
     def search_taishanese(self, query):
         data = {
